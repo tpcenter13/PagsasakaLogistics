e8cc6dcd16e29c8ee138f8341c67beaea2107f0d
unknown <tpcenter13@gmail.com>
2024-12-15 22:46:23 +0800

    layout

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Slot } from 'expo-router';
+
+const RootLayout = () => {
+    return (
+        <>
+            
+            <Slot />
+            
+            
+        </>
+    );
+};
+
+export default RootLayout;
